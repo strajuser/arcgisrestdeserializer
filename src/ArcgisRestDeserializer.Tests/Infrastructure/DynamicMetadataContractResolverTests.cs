96ac474ac934cbd40c63841c142558d761bdddb4
strajuser <marcusstrajuser@gmail.com>
2014-12-04 15:13:43 +0300

    Added NuGet References. First Version of DynamicMetadataContractResolver

---
+++ src/ArcgisRestDeserializer.Tests/Infrastructure/DynamicMetadataContractResolverTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.Serialization;
+using ArcgisRestDeserializer.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace ArcgisRestDeserializer.Tests.Infrastructure
+{
+    [TestClass]
+    public class DynamicMetadataContractResolverTests
+    {
+        [TestMethod]
+        public void TestSimpleContractResolve()
+        {
+            const string data = "{\"name\":\"Test Item\", \"value\":5}";
+
+            var resolver = new DynamicMetadataContractResolver();
+            resolver.RegisterTypeMetadata<Item, ItemMetadata>();
+            var item = JsonConvert.DeserializeObject<Item>(data, new JsonSerializerSettings
+            {
+                ContractResolver = resolver
+            });
+
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Name, "Test Item");
+            Assert.AreEqual(item.Value, 5);
+        }
+
+        #region | Nested Item Classes |
+
+        public class Item
+        {
+            public string Name { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        [DataContract]
+        public class ItemMetadata
+        {
+            [DataMember(Name = "name")]
+            public string Name { get; set; }
+            [DataMember(Name = "value")]
+            public int Value { get; set; }
+        }
+
+        #endregion
+    }
+}
---
+++ src/ArcgisRestDeserializer/Infrastructure/DynamicMetadataContractResolver.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ArcgisRestDeserializer.Infrastructure
+{
+    /// <summary>
+    /// Special DefaultContractResolver for Json.NET that resolve properties with metadata from another class
+    /// </summary>
+    public class DynamicMetadataContractResolver : DefaultContractResolver
+    {
+        private readonly Dictionary<Type, Type> _metadataDictionaty = new Dictionary<Type, Type>();
+
+        /// <summary>
+        /// Register special type with metadata for json deserializing type
+        /// </summary>
+        /// <param name="type">Deserializing Type</param>
+        /// <param name="typeMetadata">Metadata Type</param>
+        /// <returns></returns>
+        public DynamicMetadataContractResolver RegisterTypeMetadata(Type type, Type typeMetadata)
+        {
+            if (_metadataDictionaty.ContainsKey(type))
+                _metadataDictionaty[type] = typeMetadata;
+            else _metadataDictionaty.Add(type, typeMetadata);
+            return this;
+        }
+
+        /// <summary>
+        /// Register special type with metadata for json deserializing type
+        /// </summary>
+        /// <typeparam name="T">Deserializing Type</typeparam>
+        /// <typeparam name="TMetadata">Metadata Type</typeparam>
+        /// <returns></returns>
+        public DynamicMetadataContractResolver RegisterTypeMetadata<T, TMetadata>()
+        {
+            return RegisterTypeMetadata(typeof(T), typeof(TMetadata));
+        }
+
+        /// <summary>
+        /// Creates JsonProperty from metadata type
+        /// </summary>
+        /// <param name="member"></param>
+        /// <param name="memberSerialization"></param>
+        /// <returns></returns>
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty property;
+            if (!_metadataDictionaty.ContainsKey(member.DeclaringType))
+                property = base.CreateProperty(member, memberSerialization);
+            else
+            {
+                var metadataType = _metadataDictionaty[member.DeclaringType];
+                var metadataMemberInfo = metadataType.GetMember(member.Name).First();
+                property = base.CreateProperty(metadataMemberInfo, memberSerialization);
+                property.DeclaringType = member.DeclaringType;
+                property.ValueProvider = new ReflectionValueProvider(member);
+            }
+            return property;
+        }
+
+        /// <summary>
+        /// Creates list of JsonProperties from metadata type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="memberSerialization"></param>
+        /// <returns></returns>
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            IList<JsonProperty> properties;
+            if (!_metadataDictionaty.ContainsKey(type))
+                properties = base.CreateProperties(type, memberSerialization);
+            else
+            {
+                var metadataType = _metadataDictionaty[type];
+                properties = base.CreateProperties(type, memberSerialization);
+                var metadataProperties = base.CreateProperties(metadataType, memberSerialization);
+                properties = properties.Where(x => metadataProperties.Select(y => y.UnderlyingName).Contains(x.UnderlyingName)).ToList();
+            }
+            return properties;
+        }
+    }
+}
