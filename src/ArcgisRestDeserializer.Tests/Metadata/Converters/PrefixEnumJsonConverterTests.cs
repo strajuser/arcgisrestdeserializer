d3d1b5ae14e18b2d50c7c7170429b039dca5ca62
strajuser <marcusstrajuser@gmail.com>
2014-12-05 14:44:31 +0300

    Added PrefixEnumJsonConverter for converting enums with prefixes.

---
+++ src/ArcgisRestDeserializer.Tests/Metadata/Converters/PrefixEnumJsonConverterTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Media;
+using ArcgisRestDeserializer.Metadata.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace ArcgisRestDeserializer.Tests.Metadata.Converters
+{
+    [TestClass]
+    public class PrefixEnumJsonConverterTests
+    {
+        [TestMethod]
+        public void TestSimplePrefix()
+        {
+            string data = "{\"Value\":\"esriValue1\"}";
+            var rez = JsonConvert.DeserializeObject<Item>(data, new PrefixEnumJsonConverter("esri"));
+            Assert.AreEqual(rez.Value, TestEnum.Value1);
+
+            data = "{\"Value\":\"esriValue2\"}";
+            rez = JsonConvert.DeserializeObject<Item>(data, new PrefixEnumJsonConverter("esri"));
+            Assert.AreEqual(rez.Value, TestEnum.Value2);
+        }
+
+        [TestMethod]
+        public void TestIgnoreCase()
+        {
+            var data = "{\"Value\":\"esrivalue2\"}";
+            var rez = JsonConvert.DeserializeObject<Item>(data, new PrefixEnumJsonConverter("esri", true));
+            Assert.AreEqual(rez.Value, TestEnum.Value2);
+        }
+
+        #region | Nested Classes |
+
+        public enum TestEnum
+        {
+            Value1,
+            Value2
+        }
+
+        public class Item
+        {
+            //[JsonConverter(typeof(PrefixEnumJsonConverter), "esri")]
+            public TestEnum Value { get; set; }
+        }
+
+        #endregion
+    }
+}
---
+++ src/ArcgisRestDeserializer/Metadata/Converters/PrefixEnumJsonConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ArcgisRestDeserializer.Metadata.Converters
+{
+    /// <summary>
+    /// Convert strings with prefix to Enum (ex. esriSMSCircle -> SimpleMarkerSymbol.SimpleMarkerStyle.Circle)
+    /// </summary>
+    public class PrefixEnumJsonConverter : JsonConverter
+    {
+        /// <summary>
+        /// Gets or sets additional prefix for enums
+        /// </summary>
+        public string Prefix { get; set; }
+
+        /// <summary>
+        /// Gets or sets if converter should ignore case
+        /// </summary>
+        public bool IgnoreCase { get; set; }
+
+        public PrefixEnumJsonConverter(string prefix = null, bool ignoreCase = false) : this(prefix)
+        {
+            IgnoreCase = ignoreCase;
+        }
+
+        public PrefixEnumJsonConverter(string prefix = null)
+        {
+            Prefix = prefix;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsEnum;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var prop = serializer.Deserialize<JValue>(reader);
+            return Enum.Parse(objectType, prop.Value<string>().Replace(Prefix, ""), IgnoreCase);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
